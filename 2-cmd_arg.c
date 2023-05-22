1f286e05172cede42070da67886b966504f6c50d
Lionel <ndabagajeanlionel@gmail.com>
2023-05-22 16:24:21 +0000

    Separated command lines with arguments task 2

---
+++ 2-cmd_arg.c
@@ -0,0 +1,44 @@
+/**
+ * cmdtoargv - Converts a command string to an argument vector.
+ * @line: The command string to be converted.
+ *
+ * Return: The argument vector.
+ */
+char **cmdtoargv(char *line)
+{
+	char **argv;
+	char *str, *token, *delim = " \n";
+	int i = 0, len;
+
+	if (line == NULL)
+		return (NULL);
+
+	argv = malloc(SIZE * sizeof(char *));
+	if (argv == NULL)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+
+	str = strdup(line);
+	if (str != NULL)
+	{
+		token = strtok(str, delim);
+		while (token)
+		{
+			len = strlen(token) + 1;
+			argv[i] = malloc(len * sizeof(char));
+			if (argv[i] == NULL)
+			{
+				perror("malloc");
+				return (NULL);
+			}
+			snprintf(argv[i], len, "%s", token);
+			i++;
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+	}
+
+	return (argv);
+}
--- getline.c
+++ getline.c
@@ -60,51 +60,6 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 }
 
 /**
- * cmdtoargv - Converts a command string to an argument vector.
- * @line: The command string to be converted.
- *
- * Return: The argument vector.
- */
-char **cmdtoargv(char *line)
-{
-	char **argv;
-	char *str, *token, *delim = " \n";
-	int i = 0, len;
-
-	if (line == NULL)
-		return (NULL);
-
-	argv = malloc(SIZE * sizeof(char *));
-	if (argv == NULL)
-	{
-		perror("malloc");
-		return (NULL);
-	}
-
-	str = strdup(line);
-	if (str != NULL)
-	{
-		token = strtok(str, delim);
-		while (token)
-		{
-			len = strlen(token) + 1;
-			argv[i] = malloc(len * sizeof(char));
-			if (argv[i] == NULL)
-			{
-				perror("malloc");
-				return (NULL);
-			}
-			snprintf(argv[i], len, "%s", token);
-			i++;
-			token = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-	}
-
-	return (argv);
-}
-
-/**
  * _execve - Executes a command by forking a new process.
  * @argv: The argument vector containing the command and its arguments.
  *
