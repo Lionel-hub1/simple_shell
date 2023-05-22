c04502ab54292bfd3f4b7c8d2857bb66a95050b1
Lionel <ndabagajeanlionel@gmail.com>
2023-05-22 15:18:13 +0000

    Comments in main

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,10 @@
 #include "shell.h"
 
+/**
+ * main - Entry point of the shell program
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	char *line = NULL;
@@ -20,11 +25,14 @@ int main(void)
                 if (strcmp(line, "exit\n") == 0)
                         return (0);
 
+		/* Call _printenv function to print environment variables */
                 if (strcmp(line, "printenv\n") == 0 ||
                                 strcmp(line, "env\n") == 0)
                         _printenv();
+		/* Call _clear function to clear the terminal screen */
                 if (strcmp(line, "clear\n") == 0)
                         _clear();
+		/* Call _execve function to execute the command with arguments */
                 else
                 {
                         if (strcmp(line, "\n") != 0)
