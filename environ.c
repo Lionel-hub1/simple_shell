e5ba5c41896e79f5a828196501e120879aed5088
Lionel <ndabagajeanlionel@gmail.com>
2023-05-22 17:16:22 +0000

    moved which

--- environ.c
+++ environ.c
@@ -1,36 +1,6 @@
 #include "shell.h"
 
 /**
- * _which - is function to find file path;
- * @filename: is a file to be used;
- *
- * Return: filepath otherwise NULL if not found.
- */
-char *_which(const char *filename)
-{
-	char *filepath = malloc(SIZE);
-	const char *path = _getenv("PATH");
-	char *pathcp = strdup(path);
-	char *dir = strtok(pathcp, ":");
-
-	while (dir)
-	{
-		snprintf(filepath, SIZE, "%s/%s", dir, filename);
-		if (access(filepath, F_OK) == 0)
-		{
-			free(pathcp);
-			return (filepath);
-		}
-		dir = strtok(NULL, ":");
-	}
-
-	free(filepath);
-	free(pathcp);
-
-	return (NULL);
-}
-
-/**
  * _getenv - is a function to find environment of a given name;
  * @name: is name to find it's value in environment;
  *
